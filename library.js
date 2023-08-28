58b996a662f01c09ea4f38e3cd7b04ffb617b858
sujit-coditas <sujit.maiti@coditas.com>
2023-08-28 18:21:48 +0530

    fix: add error handling for all possible case - Invalid book details in book class - Input validation in library class - Handling empty search results - Improving error handling in main

--- library.js
+++ library.js
@@ -1,22 +1,50 @@
+const Book = require("./book");
+
 class Library {
   constructor() {
     this.books = [];
   }
 
   addBook(book) {
-    this.books.push(book);
+    if (book instanceof Book) {
+      this.books.push(book);
+    } else {
+      throw new Error(
+        "Only instances of the Book class can be added to the library."
+      );
+    }
   }
 
   searchByTitle(title) {
-    return this.books.filter((book) =>
+    if (typeof title !== "string") {
+      throw new Error("Invalid input type for search.");
+    }
+
+    const results = this.books.filter((book) =>
       book.title.toLowerCase().includes(title.toLowerCase())
     );
+
+    if (results.length === 0) {
+      console.log("No books found for the given title.");
+    }
+
+    return results;
   }
 
   searchByAuthor(author) {
-    return this.books.filter((book) =>
+    if (typeof author !== "string") {
+      throw new Error("Invalid input type for search.");
+    }
+
+    const results = this.books.filter((book) =>
       book.author.toLowerCase().includes(author.toLowerCase())
     );
+
+    if (results.length === 0) {
+      console.log("No books found for the given author.");
+    }
+
+    return results;
   }
 
   getTotalBooks() {
